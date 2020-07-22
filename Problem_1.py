f19c949779d4f46357a03c1462a1b858625b0e2c
wattun <peanotsp0430@gmail.com>
2020-07-22 17:32:32 +0900

    問題1についてのコメントを追記

--- Problem_1.py
+++ Problem_1.py
@@ -1,14 +1,13 @@
-# Problem 1
+# Problem 1 : Multiples of 3 or 5
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-# 問題1
+# 問題 1 : 3もしくは5の倍数
 # 10以下の自然数において3もしくは5の倍数は3, 5, 6, 9である．
 # そしてそれらの合計は23である．
 # 1000以下の3もしくは5の倍数の合計値を求めよ．
 
-Find the sum of all the multiples of 3 or 5 below 1000.
 total = 0
 i = 1
 
