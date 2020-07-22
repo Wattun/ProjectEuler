5f48cde1081904740f0f84e7d27c548fc8767211
wattun <peanotsp0430@gmail.com>
2020-07-22 17:32:57 +0900

    問題3についてのコメントを追記

--- Problem_3.py
+++ Problem_3.py
@@ -1,3 +1,11 @@
+# Problem 3 : Largest prime factor
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+# 問題 3 : 最大素因数
+# 13195の素因数は5, 7, 13, 29である．
+# 600851475143の素因数はいくらか？
+
 i = 2
 Composite_number = 600851475143
 prime_list = []
