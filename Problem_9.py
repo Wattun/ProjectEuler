a0b3edbdeb4fb792a7759311dfe5f3e62c25c6da
wattun <peanotsp0430@gmail.com>
2020-07-28 17:07:51 +0900

    問題文を追加

---
+++ Problem_9.py
@@ -0,0 +1,14 @@
+# Problem 9 : Special Pythagorean triplet
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+# 問題 9 : 特別なピタゴラス数
+# ピタゴラス数とは下の式が成立し，a < b < cが成り立つ3つの数字である．
+# a^2 + b^2 = c^2
+# 例えば，3^2 + 4^2 = 9 + 16 = 25 = 5^2である．
+# a + b + c = 1000となるピタゴラス数が一組だけ存在する．
+# ピタゴラス数a, b, cの積abcを求めよ．
+
