64b5d0b997c2ff3a8a685afbde7d48834b591181
wattun <peanotsp0430@gmail.com>
2020-11-06 17:24:23 +0900

    問題10の問題文を追加

---
+++ Problem_10.py
@@ -0,0 +1,8 @@
+# Problem 10 : Summation of primes
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+# 問題 10 :  素数の総和
+# 10以下の素数の和は2+3+5+7=17のようになる．
+# 200万以下の全ての素数の総和を求めよ．
+
